eb98568cfcbae67727bac0ee9b5046d9ef45a76a
chrismargo <78247006+chrismargo@users.noreply.github.com>
2022-07-16 15:54:12 +0800

    Finished buttons on top navigation bar

--- index.php
+++ index.php
@@ -1,44 +1,21 @@
 <?php
-
+    include "top_nav.php";
 ?>
-
-<!DOCTYPE html>
+<!doctype html>
 <html lang="en">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+  </head>
+  <body>
+    <!-- START CODE HERE -->
+    
 
-        <title>BetterPrints </title>
-
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
-        <!-- <link rel="stylesheet" href="assets/css/sample.css"/> -->
-    </head>
-    <body>
-        <!-- START CODE HERE -->
-        <h1>Hello, World</h1>
-        <style>
-            [class*="col"]{
-                padding: 1rem;
-                background-color: #33b5e5;
-                border: 2px solid #fff;
-                color: #fff;
-            }
-            </style>
-
-        <div class="container-md border">
-            <div class="row">
-                <div class="col">Col1</div>
-                <div class="col">Col2</div>
-                <div class="col">Col3</div>
-                <div class="col">Col4</div>
-                <div class="col">Col5</div>
-            </div>
-        </div>
-
-        <!-- END CODE HERE -->
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
-        <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.5/dist/umd/popper.min.js" integrity="sha384-Xe+8cL9oJa6tN/veChSP7q+mnSPaj5Bcu9mPX5F5xIGE0DVittaqT5lorf0EI7Vk" crossorigin="anonymous"></script>
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.min.js" integrity="sha384-kjU+l4N0Yf4ZOJErLsIcvOU2qSb74wXpOhqTvwVx3OElZRweTnQ6d31fXEoRD1Jy" crossorigin="anonymous"></script>
-        <!-- <script src="assets/js/boostrap.js"></script> -->
-    </body>
+    <!-- END CODE HERE -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.5/dist/umd/popper.min.js" integrity="sha384-Xe+8cL9oJa6tN/veChSP7q+mnSPaj5Bcu9mPX5F5xIGE0DVittaqT5lorf0EI7Vk" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.min.js" integrity="sha384-kjU+l4N0Yf4ZOJErLsIcvOU2qSb74wXpOhqTvwVx3OElZRweTnQ6d31fXEoRD1Jy" crossorigin="anonymous"></script>
+  </body>
 </html>
--- top_nav.php
+++ top_nav.php
@@ -9,20 +9,25 @@
     <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Bootstrap demo</title>
+    <title>Top Navigation Bar</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+    <!-- EXTERNAL CSS FILE -->
+    <!-- <link href="assets/css/top_nav.css" rel="stylesheet"> -->
     </head>
     <body>
     <!-- START CODE HERE -->
 
-    <nav class="navbar navbar-expand-lg bg-light bg-transparent">
+    <nav class="navbar navbar-expand-lg bg-light bg-transparent ps-3">
         <div class="container-fluid">
-            <a class="navbar-brand" href="#">Navbar w/ text</a>
+            <a class="navbar-brand" href="#">
+                <img src="assets/logo/bp-logo-transparent.png" alt="" width="80" height="70">
+            </a>
+            <!-- <a class="navbar-brand ps-2" href="#">BetterPrints</a> -->
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarText">
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <!-- <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                 <li class="nav-item">
                 <a class="nav-link active" aria-current="page" href="#">Home</a>
                 </li>
@@ -32,10 +37,31 @@
                 <li class="nav-item">
                 <a class="nav-link" href="#">Pricing</a>
                 </li>
-            </ul>
-            <span class="navbar-text">
+                <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    Dropdown link
+                </a>
+                <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                    <li><a class="dropdown-item" href="#">Action</a></li>
+                    <li><a class="dropdown-item" href="#">Another action</a></li>
+                    <li><a class="dropdown-item" href="#">Something else here</a></li>
+                </ul>
+                </li>
+            </ul> -->
+            <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+            <!-- <li class="nav-item">
+                <a class="nav-link" aria-current="page" href="#">Hello, Jemma</a>
+            </li> -->
+
+            <button type="button" class="btn btn-outline-success"  style="--bs-btn-padding-y: .1rem; --bs-btn-padding-x: .5rem; --bs-btn-font-size: .75rem;">Go to Main Page</button>
+            <span class="navbar-text ps-4 pe-4">
                 Hello, Jemma
             </span>
+            <li class="nav-item">
+                <a class="nav-link pe-4" aria-current="page" href="#">Settings</a>
+            </li>
+            <button class="btn btn-outline-danger btn-sm px-3 rounded-pill" style="--bs-btn-padding-y: .1rem; --bs-btn-padding-x: .5rem; --bs-btn-font-size: .75rem;" type="logout">Logout</button>
+            </ul>
             </div>
         </div>
     </nav>
