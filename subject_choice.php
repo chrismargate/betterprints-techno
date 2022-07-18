772c557bc3a852c7832ee1a4d5da44792a4264e9
chrismargo <78247006+chrismargo@users.noreply.github.com>
2022-07-18 16:35:48 +0800

    added video background on subject choice

--- subject_choice.php
+++ subject_choice.php
@@ -13,8 +13,8 @@
   <meta content="" name="keywords">
 
   <!-- Favicons -->
-  <link href="assets/img/favicon.png" rel="icon">
-  <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+  <!-- <link href="assets/img/favicon.png" rel="icon">
+  <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon"> -->
 
   <!-- Google Fonts -->
   <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,700,700i|Poppins:300,400,500,700" rel="stylesheet">
@@ -34,15 +34,25 @@
 
 <body>
 
+<!-- <div class="overlay"></div> -->
+
+
+
+
   <!-- ======= Hero Section ======= -->
   <section id="hero">
     <div class="hero-container" data-aos="zoom-in" data-aos-delay="100">
+     
       <h1>Welcome to our group website!</h1>
       <h2>Please choose a subject:</h2>
-      <a href="#about" class="btn-get-started">Technopreneurship</a>
-      <a href="#about" class="btn-get-started">Information Assurance & Security</a>
+      <a href="#about" class="btn-get-started">Technopreneurship - BetterPrints</a>
+      <a href="#about" class="btn-get-started">Information Assurance & Security - ACare Appointment System</a>
+      <video class="back-video" playsinline="playsinline" autoplay="autoplay" muted="muted" loop="loop">
+        <source src="assets/video/sunset-woods.mp4" type="video/mp4">
+      </video>
     </div>
-  </section><!-- End Hero Section -->
+  </section>
+  <!-- End Hero Section -->
 
   <!-- Vendor JS Files -->
   <script src="assets/vendor/purecounter/purecounter.js"></script>
