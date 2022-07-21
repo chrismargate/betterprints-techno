091efe3b7f2645e039b32f924185320225dd46ef
margate23 <78247006+chrismargo@users.noreply.github.com>
2022-07-21 12:12:50 +0800

    modified index.php and added pricing pictures

--- index.php
+++ index.php
@@ -378,6 +378,27 @@
           </div>
         </div>
       </div>
+      <div class="container" data-aos="fade-up">
+        <!-- <div class="section-title text-center">
+          <h2>App Gallery</h2>
+          <p class="separator">Integer cursus bibendum augue ac cursus .</p>
+        </div> -->
+        <section id="screenshots" class="padd-section text-center">
+          <div class="screens-slider swiper">
+            <div class="swiper-wrapper align-items-center">
+                <div class="swiper-slide"><img src="assets/img/screen/paper-1.png" class="img-fluid" alt=""></div>
+                <div class="swiper-slide"><img src="assets/img/screen/paper-2.png" class="img-fluid" alt=""></div>
+                <div class="swiper-slide"><img src="assets/img/screen/paper-3.png" class="img-fluid" alt=""></div>
+                <div class="swiper-slide"><img src="assets/img/screen/paper=4.png" class="img-fluid" alt=""></div>
+                <div class="swiper-slide"><img src="assets/img/screen/cards-1.png" class="img-fluid" alt=""></div>
+                <div class="swiper-slide"><img src="assets/img/screen/tarpaulin-1.png" class="img-fluid" alt=""></div>
+                <div class="swiper-slide"><img src="assets/img/screen/shirt-1.png" class="img-fluid" alt=""></div>
+                <div class="swiper-slide"><img src="assets/img/screen/8.jpg" class="img-fluid" alt=""></div>
+            </div>
+            <div class="swiper-pagination"></div>
+            </div>
+          </div>
+        </section>
     </section>
     <!-- End Pricing Section -->
 
